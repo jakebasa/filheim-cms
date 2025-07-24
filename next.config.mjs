e94d60db031c18c9064842382610441a2635e117
devjeiku <jakebasa0428@gmail.com>
2025-07-24 18:28:10 +0800

    fix: fix css related issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    optimizeFonts: true,
-    compiler: {
-        removeConsole: process.env.NODE_ENV === 'production',
-    },
-    // Optimize CSS loading
-    experimental: {
-        optimizeCss: true,
-        optimizeServerReact: true,
-    },
-    // Improve static asset handling
-    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://filheim.com' : '',
-    staticPageGenerationTimeout: 120,
-};
+const nextConfig = {};
 
 export default nextConfig;
